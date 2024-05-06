a67776f53fd5fce1d76fdab2d6a9cdbf5b06d3b3
code-death <poorvanshkavta938@gmail.com>
2024-05-06 23:06:18 +0530

    added charts

--- src/pages/Home/components/Graphs.jsx
+++ src/pages/Home/components/Graphs.jsx
@@ -1,7 +1,41 @@
+import {useEffect, useState} from "react";
+import callApi from "../../../utility/apiCaller.js";
+import Chart from 'react-apexcharts'
 const Graphs = () => {
+    const [stats, setStats] = useState([]);
+    const [options, setOptions] = useState({
+        chart: {
+            id: 'apexchart-example'
+        },
+        labels: [],
+    })
+
+    useEffect(() => {
+        callApi('transactions/stats', 'get').then(res => {
+            if(res.status === 'Success') {
+                setStats(res?.data?.transactionStats.map(stat => stat.count))
+                setOptions((prev) => ({
+                    chart: prev.chart,
+                    labels: res.data.transactionStats.map(stat => stat._id)
+                }))
+            } else {
+
+            }
+        }).catch(e => {
+            console.log(e)
+        })
+    }, []);
+
     return (
-        <div>
-            Graphs
+        <div style={{display: "flex", justifyContent: 'center', alignItems: 'center'}}>
+            <h3>Status %</h3>
+            <Chart
+                options={options}
+                type={'pie'}
+                series={stats}
+                width={'500px'}
+                height={'500px'}
+            />
         </div>
     )
 }
